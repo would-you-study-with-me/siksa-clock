06e84151125712f5f00370dbc93dfe14209d6b61
dalsa <dalsa@jaranda.kr>
2022-06-14 21:07:20 +0900

    make

--- back-end/auth-api/app/services/token.py
+++ back-end/auth-api/app/services/token.py
@@ -1,7 +1,6 @@
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 
