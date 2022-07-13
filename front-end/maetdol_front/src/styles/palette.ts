951a43b6967381bcd36aae1639cc4892eaf0721c
MaetDol <rndka9@gmail.com>
2022-07-13 22:35:00 +0900

    #26 컬러팔레트 선언

---
+++ front-end/maetdol_front/src/styles/palette.ts
@@ -0,0 +1,12 @@
+const colors = {
+  fontBlack: '#212121',
+  gray: '#B7B7B7',
+  white: '#F2F2F2',
+  primaryRed: '#E42021',
+  primaryLight: '#F56566',
+  primaryDark: '#C80C0D',
+  green: '#4EC03B',
+  yellow: '#EDD068',
+} as const;
+
+export default colors;
