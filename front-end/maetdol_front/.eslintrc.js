ef12c23b48a9e46b8060d0641d070f2f50a807d0
MaetDol <rndka9@gmail.com>
2022-07-23 11:04:54 +0900

    리액트 import 를 생략 가능하게 변경

--- front-end/maetdol_front/.eslintrc.js
+++ front-end/maetdol_front/.eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }], // 확장자로 ts tsx 허용하도록 수정
     'import/extensions': 0, // import 경로 에러 해제
     'import/no-unresolved': 0, // import 경로 에러 해제
+    'react/react-in-jsx-scope': 0, // import React from 'react'; 를 생략 가능하게, 에러 해제
   },
 };
