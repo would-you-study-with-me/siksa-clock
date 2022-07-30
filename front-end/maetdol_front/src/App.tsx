8d57652a617e8f27058666975bfb62a36d09442e
MaetDol <rndka9@gmail.com>
2022-07-30 12:06:43 +0900

    [#31] 글로벌 스타일 적용

--- front-end/maetdol_front/src/App.tsx
+++ front-end/maetdol_front/src/App.tsx
@@ -1,10 +1,11 @@
-import { ThemeProvider } from '@emotion/react';
+import globalStyle from 'styles/global';
 import colors from 'styles/palette';
 import typography from 'styles/typography';
 
 function App() {
   return (
     <ThemeProvider theme={{ colors, typography }}>
+      <Global styles={globalStyle} />
       <div>It works!</div>
     </ThemeProvider>
   );
