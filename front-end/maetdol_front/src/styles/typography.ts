68b6307e9cd4ccdb08bc6b322d86c7b482c611c5
MaetDol <rndka9@gmail.com>
2022-08-01 23:27:39 +0900

    잘못 설정된 line-height 설정
    index.html 에 link 추가

--- front-end/maetdol_front/src/styles/typography.ts
+++ front-end/maetdol_front/src/styles/typography.ts
@@ -1,52 +1,44 @@
-const FONT_FAMILY = 'font-family: Roboto, NanumGothic, sans-serif';
+const FONT_FAMILY = 'font-family: Roboto, NanumGothic, sans-serif;';
 
 const typography = {
   head1: `
     ${FONT_FAMILY} 
     font-size: 2.4rem; 
-    line-height: 1.5; 
     font-weight: 600;
     `,
   head2: `
     ${FONT_FAMILY} 
     font-size: 1.8rem; 
-    line-height: 1.5; 
     font-weight: 600;
     `,
   title1: `
     ${FONT_FAMILY} 
     font-size: 1.6rem; 
-    line-height: 1.5; 
     font-weight: 600;
     `,
   body1: `
     ${FONT_FAMILY} 
     font-size: 1.4rem; 
-    line-height: 1.5; 
     font-weight: 400;
     `,
   body2: `
     ${FONT_FAMILY} 
     font-size: 1.2rem; 
-    line-height: 1.5; 
     font-weight: 300;
     `,
   caption: `
     ${FONT_FAMILY} 
     font-size: 1rem; 
-    line-height: 1.5; 
     font-weight: 300;
     `,
   placeholder: `
     ${FONT_FAMILY} 
     font-size: 1rem; 
-    line-height: 1.5; 
     font-weight: 300;
     `,
   button: `
     ${FONT_FAMILY}
     font-size: 1.4rem;
-    line-height: 1.5;
     font-weight: 600;
   `,
 } as const;
