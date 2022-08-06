a038a12576e5284fe0ba1768cea09ecc7b6bf3f3
Hyeon <axzswq@gmail.com>
2022-08-06 20:20:25 +0900

    create: websocket 및 router 열기

--- back-end/mvp/app/main.py
+++ back-end/mvp/app/main.py
@@ -4,14 +4,15 @@ from sqlalchemy.orm import Session
 from app.config.database import Base
 from app.config.database import engine
 from app.dependencies import get_db
-from app.routers.restaurant import restaurants_graphql_app
+from app.routers.restaurant import restaurant_graphql_app
 from app.services.restaurants import RestaurantService
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(version=0.1)
+app = FastAPI(version=0.2)
 
-# app.include_router(restaurants_graphql_app, prefix='/restaurants')
+app.include_router(restaurant_graphql_app, prefix='/restaurant')
+app.add_websocket_route('/restaurant', restaurant_graphql_app)
 
 
 @app.get('/')
