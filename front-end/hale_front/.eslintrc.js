bfe3eae5f515af58b63a222f9c6ee88a871a293e
aawlgus <aawlgus@jaranda.kr>
2022-08-13 12:52:44 +0900

    chore: update eslint config

--- front-end/hale_front/.eslintrc.js
+++ front-end/hale_front/.eslintrc.js
@@ -20,5 +20,15 @@ module.exports = {
       2,
       { namedComponents: 'arrow-function' },
     ],
+    'react/react-in-jsx-scope': 0, // import React 생략
+    'no-shadow': 0, // Enum 버그로 인해 해당 기능 비활성화
+    'no-restricted-exports': ['error', { restrictedNamedExports: ['default'] }], // export { default } 를 사용하기 위해 허용
+    'import/prefer-default-export': 0, // 하나만 있더라도 named export 가능하게
+    'react/require-default-props': [
+      'error',
+      {
+        functions: 'defaultArguments', // 타입스크립트는 type 으로 옵셔널을 지정해줄 수 있기 때문에..
+      },
+    ],
   },
 };
