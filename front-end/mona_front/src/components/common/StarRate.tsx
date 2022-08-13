b8ca6c7f82988e708e4a96c98b5da4354ef0ea59
SoheeP <psh950502@naver.com>
2022-08-13 13:31:28 +0900

    feat: 아이콘 사이즈 props변경

--- front-end/mona_front/src/components/common/StarRate.tsx
+++ front-end/mona_front/src/components/common/StarRate.tsx
@@ -1,16 +1,20 @@
 import StarIcon from '../../assets/icons/StarIcon';
 
+type Size = 'inherit' | 'large' | 'medium' | 'small';
 interface Props extends React.PropsWithChildren {
   rate: number;
+  size?: Size;
 }
 
-const StarRate = ({ rate }: Props) => {
+const StarRate = ({ rate, ...props }: Props) => {
+  const { size = 'small' } = props;
   const makeStarIcon = new Array(5)
     .fill(<StarIcon />)
     .map((item, index) => (
       <StarIcon
         key={`starIcon-${index + 100}`}
         color={rate > index ? 'primary' : 'disabled'}
+        fontSize={size}
       />
     ));
   return <div>{makeStarIcon}</div>;
