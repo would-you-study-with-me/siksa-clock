1db4f0cbdfe4ef0ac1f1dbd2d52f3b19e2948bda
MaetDol <rndka9@gmail.com>
2022-08-27 12:31:17 +0900

    다음 Postcode 로딩 기다리고 실행하는 api 작성

---
+++ front-end/maetdol_front/src/api/daum.ts
@@ -0,0 +1,20 @@
+import { promisify } from 'utils/promise';
+
+const [loadDaumPostcode, load, fail] = promisify<void, string>();
+
+const loading = setInterval(() => {
+  if (typeof daum !== 'undefined') {
+    load();
+    clearInterval(loading);
+  }
+}, 999);
+
+const DAUM_POSTCODE_TIMEOUT = 9 * 1000;
+setTimeout(() => {
+  fail('daum postcode timeout');
+  clearInterval(loading);
+}, DAUM_POSTCODE_TIMEOUT);
+
+export function getPostcode(option: daum.ConstructorProps = {}) {
+  return loadDaumPostcode.then(() => new daum.Postcode(option));
+}
