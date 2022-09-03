5c0036a3df91a7cc631857c42b5472e6e769c0e8
aawlgus <aawlgus@jaranda.kr>
2022-09-03 12:06:17 +0900

    feat: create restaurant card component

--- front-end/hale_front/src/components/RestaurantCard.tsx
+++ front-end/hale_front/src/components/RestaurantCard.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import { Card, CardContent } from '@mui/material';
+import { RestaurantCardInfo } from '../model/restaurant-card.interface';
 
-function RestaurantCard() {
-  return <div>card works!</div>;
-}
+type RestaurantCardProps = {
+  restaurantInfo: RestaurantCardInfo;
+  // onClick?: () => void;
+};
+
+const RestaurantCard = ({ restaurantInfo }: RestaurantCardProps) => {
+  return (
+    <div>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <img
+            alt="restaurant thumbnail"
+            style={{ backgroundPosition: 'center' }}
+          />
+          <Typography variant="subtitle1">
+            {restaurantInfo.restaurantName}
+          </Typography>
+          <div>평점: {restaurantInfo.restaurantRate}</div>
+          <div>카테고리: {restaurantInfo.restaurantCategory}</div>
+          <div>혼잡도: {restaurantInfo.restaurantCongestion}</div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default RestaurantCard;
--- front-end/hale_front/src/pages/Main.tsx
+++ front-end/hale_front/src/pages/Main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Typography from '@mui/material/Typography';
+import RestaurantCard from '../components/RestaurantCard';
+import { RestaurantCardInfo } from '../model/restaurant-card.interface';
 
 const GET_RESTAURANTS = gql`
   query GetRestaurants {
@@ -16,13 +18,19 @@ const GET_RESTAURANTS = gql`
 `;
 
 const Main: React.FC = () => {
-  const { loading, error, data } = useQuery(GET_RESTAURANTS);
+  const { loading, error, data } = useQuery<{
+    mockRestaurants: RestaurantCardInfo[];
+  }>(GET_RESTAURANTS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
     <div>
       <Typography variant="h2">내 주변 식사</Typography>
+      {data &&
+        data.mockRestaurants.map(restaurant => (
+          <RestaurantCard restaurantInfo={restaurant} />
+        ))}
     </div>
   );
 };
