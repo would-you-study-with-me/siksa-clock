8542a33b535434c1d9fda6d66b84930ef5e13917
Hyeon <axzswq@gmail.com>
2022-09-14 22:52:17 +0900

    delete: 리펙토링을 위해 필요없는 데이터 삭제

--- back-end/mvp/app/routers/restaurant.py
+++ back-end/mvp/app/routers/restaurant.py
@@ -8,24 +8,9 @@ from strawberry.fastapi import GraphQLRouter
 from app.schemas.restaurant import RestaurantAll
 
 
-# TODO: 나중에 분리
-async def resolve_hellow(root) -> str:
-    await asyncio.sleep(1)
-    return "Hello"
-
-
 @strawberry.type
 class Query:
-
-    @strawberry.field
-    async def hello(self) -> str:
-        pass
-        # hello: str = strawberry.field(resolver=resolve_hellow)
-
-    @strawberry.field
-    def mock_restaurants(self) -> List[RestaurantAll]:
-        pass
-
+    pass
 
 schema = strawberry.Schema(
     query=Query
