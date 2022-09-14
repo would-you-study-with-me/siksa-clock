055b9309375f46756553c0abbea65f39589ce5fe
Hyeon <axzswq@gmail.com>
2022-09-14 22:53:45 +0900

    create: CORS middleware 삽입

--- back-end/mvp/app/main.py
+++ back-end/mvp/app/main.py
@@ -2,6 +2,7 @@ import asyncio
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config.database import async_main
 from app.routers.restaurant import restaurant_graphql_app
@@ -11,7 +12,20 @@ app = FastAPI(version=0.4)
 app.include_router(restaurant_graphql_app, prefix='/restaurant')
 app.add_websocket_route('/restaurant', restaurant_graphql_app)
 
-# CORS middleware
+# CORS middleware 규모가 커지면 분리
+origins = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://web.siksa-clock.kro.kr"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
