c7bd9ce63657d788fc48b7d5211a4017cccd42af
Hyeon <axzswq@gmail.com>
2022-09-18 20:31:50 +0900

    create: resolver part

---
+++ back-end/mvp/app/resolvers/restaurant.py
@@ -0,0 +1,35 @@
+import typing
+from uuid import UUID
+
+from sqlalchemy import delete, select, update
+
+from app.config.database import get_session
+from app.models.restaurant import Restaurants, OpeningTime
+
+
+async def get_restaurants() -> typing.List[Restaurants]:
+
+    async with get_session() as s:
+        sql = select(Restaurants).join(OpeningTime).order_by(Restaurants.restaurant_id)
+        db_users = (await s.execute(sql)).scalars().unique().all()
+        print(db_users)
+
+    return db_users
+
+
+async def get_restaurant(restaurant_id: UUID) -> Restaurants:
+
+    async with get_session() as s:
+        sql = select(Restaurants).join(OpeningTime).where(Restaurants.restaurant_id == restaurant_id)
+        db_user = (await s.execute(sql)).scalars().all()
+        print(db_user)
+
+    return db_user
+
+
+async def add_user():
+    pass
+
+
+async def delete_user():
+    pass
