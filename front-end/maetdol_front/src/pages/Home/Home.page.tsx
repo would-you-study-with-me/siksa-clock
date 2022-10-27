43733581f7f2b69f0b071b8120544f76ffd41029
MaetDol <rndka9@gmail.com>
2022-10-27 21:48:22 +0900

    [#90] 임시데이터로 카드리스트 보여주게 작업

--- front-end/maetdol_front/src/pages/Home/Home.page.tsx
+++ front-end/maetdol_front/src/pages/Home/Home.page.tsx
@@ -1,125 +1,48 @@
-import { Icon, StarRate } from 'components';
-import { IconFileNames } from 'components/Icon';
-import {
-  StyeldCardInformationWrapper,
-  StyeldCardList,
-  StyledCard,
-  StyledCardCategory,
-  StyledCardImage,
-  StyledCardTitle,
-  StyledCongestion,
-  StyledContainer,
-  StyledDisatnce,
-  StyledLink,
-  StyledRightContents,
-  StyledTitle,
-} from './Home.styles';
+import { ThumbnailCard } from 'components';
+import { Congestion } from 'components/ThumbnailCard/ThumbnailCard.model';
+import { StyeldCardList, StyledContainer, StyledTitle } from './Home.styles';
 
 export function Home() {
   return (
     <StyledContainer>
       <StyledTitle>내 주변 식사</StyledTitle>
       <StyeldCardList>
-        <StyledCard>
-          <StyledLink to="/">
-            <StyledCardImage
-              src="https://via.placeholder.com/150x270"
-              alt="temp"
-            />
-            <StyledCardTitle>
-              젠킨스 할아버지는 왜 달사를 괴롭히나
-            </StyledCardTitle>
-            <StyeldCardInformationWrapper>
-              <StarRate rating={4} />
-              <StyledCardCategory>양식</StyledCardCategory>
-              <StyledRightContents>
-                <StyledCongestion>혼잡도</StyledCongestion>
-                <Icon type={IconFileNames.FACE_GOOD} />
-                <StyledDisatnce>99999m</StyledDisatnce>
-              </StyledRightContents>
-            </StyeldCardInformationWrapper>
-          </StyledLink>
-        </StyledCard>
-        <StyledCard>
-          <StyledLink to="/">
-            <StyledCardImage
-              src="https://via.placeholder.com/150x270"
-              alt="temp"
-            />
-            <StyledCardTitle>
-              젠킨스 할아버지는 왜 달사를 괴롭히나
-            </StyledCardTitle>
-            <StyeldCardInformationWrapper>
-              <StarRate rating={4} />
-              <StyledCardCategory>양식</StyledCardCategory>
-              <StyledRightContents>
-                <StyledCongestion>혼잡도</StyledCongestion>
-                <Icon type={IconFileNames.FACE_GOOD} />
-                <StyledDisatnce>99999m</StyledDisatnce>
-              </StyledRightContents>
-            </StyeldCardInformationWrapper>
-          </StyledLink>
-        </StyledCard>
-        <StyledCard>
-          <StyledLink to="/">
-            <StyledCardImage
-              src="https://via.placeholder.com/150x270"
-              alt="temp"
-            />
-            <StyledCardTitle>
-              젠킨스 할아버지는 왜 달사를 괴롭히나
-            </StyledCardTitle>
-            <StyeldCardInformationWrapper>
-              <StarRate rating={4} />
-              <StyledCardCategory>양식</StyledCardCategory>
-              <StyledRightContents>
-                <StyledCongestion>혼잡도</StyledCongestion>
-                <Icon type={IconFileNames.FACE_GOOD} />
-                <StyledDisatnce>99999m</StyledDisatnce>
-              </StyledRightContents>
-            </StyeldCardInformationWrapper>
-          </StyledLink>
-        </StyledCard>
-        <StyledCard>
-          <StyledLink to="/">
-            <StyledCardImage
-              src="https://via.placeholder.com/150x270"
-              alt="temp"
-            />
-            <StyledCardTitle>
-              젠킨스 할아버지는 왜 달사를 괴롭히나
-            </StyledCardTitle>
-            <StyeldCardInformationWrapper>
-              <StarRate rating={4} />
-              <StyledCardCategory>양식</StyledCardCategory>
-              <StyledRightContents>
-                <StyledCongestion>혼잡도</StyledCongestion>
-                <Icon type={IconFileNames.FACE_GOOD} />
-                <StyledDisatnce>99999m</StyledDisatnce>
-              </StyledRightContents>
-            </StyeldCardInformationWrapper>
-          </StyledLink>
-        </StyledCard>
-        <StyledCard>
-          <StyledLink to="/">
-            <StyledCardImage
-              src="https://via.placeholder.com/150x270"
-              alt="temp"
-            />
-            <StyledCardTitle>
-              젠킨스 할아버지는 왜 달사를 괴롭히나
-            </StyledCardTitle>
-            <StyeldCardInformationWrapper>
-              <StarRate rating={4} />
-              <StyledCardCategory>양식</StyledCardCategory>
-              <StyledRightContents>
-                <StyledCongestion>혼잡도</StyledCongestion>
-                <Icon type={IconFileNames.FACE_GOOD} />
-                <StyledDisatnce>99999m</StyledDisatnce>
-              </StyledRightContents>
-            </StyeldCardInformationWrapper>
-          </StyledLink>
-        </StyledCard>
+        <ThumbnailCard
+          category="일식"
+          congestion={Congestion.CROWDED}
+          meterDistance={172}
+          rating={3}
+          restaurantId="1372-abf334"
+          thumbnailSrc="https://via.placeholder.com/150"
+          title="달사카세"
+        />
+        <ThumbnailCard
+          category="일식"
+          congestion={Congestion.CROWDED}
+          meterDistance={172}
+          rating={3}
+          restaurantId="1372-abf334"
+          thumbnailSrc="https://via.placeholder.com/150"
+          title="달사카세"
+        />
+        <ThumbnailCard
+          category="일식"
+          congestion={Congestion.CROWDED}
+          meterDistance={172}
+          rating={3}
+          restaurantId="1372-abf334"
+          thumbnailSrc="https://via.placeholder.com/150"
+          title="달사카세"
+        />
+        <ThumbnailCard
+          category="일식"
+          congestion={Congestion.CROWDED}
+          meterDistance={172}
+          rating={3}
+          restaurantId="1372-abf334"
+          thumbnailSrc="https://via.placeholder.com/150"
+          title="달사카세"
+        />
       </StyeldCardList>
     </StyledContainer>
   );
--- front-end/maetdol_front/src/pages/Home/Home.styles.ts
+++ front-end/maetdol_front/src/pages/Home/Home.styles.ts
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
 
 export const StyledContainer = styled.div`
   padding: 4rem 1.6rem;
@@ -15,56 +14,4 @@ export const StyeldCardList = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
-`;
-
-export const StyledCard = styled.li`
-  margin-bottom: 2.4rem;
-`;
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: ${props => props.theme.colors.fontBlack};
-`;
-
-export const StyledCardImage = styled.img`
-  width: 100%;
-  height: 12rem;
-  object-fit: cover;
-  object-position: center;
-  background: oragne;
-  border-radius: 8px;
-`;
-
-export const StyledCardTitle = styled.h3`
-  ${props => props.theme.typography.title1};
-  margin: 1.6rem 0 1.3rem;
-`;
-
-export const StyeldCardInformationWrapper = styled.div`
-  display: flex;
-  margin: 0;
-  align-items: center;
-`;
-
-export const StyledCardCategory = styled.span`
-  ${props => props.theme.typography.body1};
-  color: ${props => props.theme.colors.fontBlack};
-  margin-left: 0.8rem;
-`;
-
-export const StyledRightContents = styled.div`
-  margin-left: auto;
-  display: flex;
-  align-items: center;
-`;
-
-export const StyledCongestion = styled.span`
-  ${props => props.theme.typography.body1};
-  color: ${props => props.theme.colors.fontBlack};
-`;
-
-export const StyledDisatnce = styled.span`
-  ${props => props.theme.typography.body1};
-  color: ${props => props.theme.colors.fontBlack};
-  margin-left: 2.4rem;
 `;
