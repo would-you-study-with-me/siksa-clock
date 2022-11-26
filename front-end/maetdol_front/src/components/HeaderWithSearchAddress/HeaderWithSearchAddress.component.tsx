f138c3a69de09097ce0a53d42394cca0adc50106
MaetDol <rndka9@gmail.com>
2022-11-26 12:32:41 +0900

    주소 누르면 검색하는 창이 나오게끔 수정

--- front-end/maetdol_front/src/components/HeaderWithSearchAddress/HeaderWithSearchAddress.component.tsx
+++ front-end/maetdol_front/src/components/HeaderWithSearchAddress/HeaderWithSearchAddress.component.tsx
@@ -1,17 +1,24 @@
+import { DaumPostcode } from 'components';
 import Icon, { IconFileNames } from 'components/Icon';
+import { useState } from 'react';
 import colors from 'styles/palette';
 import { StyledButton, StyledHeader } from './HeaderWithSearchAddress.styles';
 
 export default function HeaderWithSearchAddress() {
+  const [showSearchAddress, setShowSearchAddress] = useState(false);
+
   return (
     <StyledHeader>
-      <StyledButton type="button">
+      <StyledButton type="button" onClick={() => setShowSearchAddress(true)}>
         <Icon
           type={IconFileNames.LOCATION_PIN}
           colors={{ black: colors.primaryDark }}
         />
         동이름(시 구)
       </StyledButton>
+      {showSearchAddress && (
+        <DaumPostcode close={() => setShowSearchAddress(false)} />
+      )}
     </StyledHeader>
   );
 }
