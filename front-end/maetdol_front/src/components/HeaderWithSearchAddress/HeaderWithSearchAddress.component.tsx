f89973609ff73cbabc6b05255ff53d1610c36865
MaetDol <rndka9@gmail.com>
2022-12-26 23:30:02 +0900

    [#69] 주소 헤더 개발 완료

--- front-end/maetdol_front/src/components/HeaderWithSearchAddress/HeaderWithSearchAddress.component.tsx
+++ front-end/maetdol_front/src/components/HeaderWithSearchAddress/HeaderWithSearchAddress.component.tsx
@@ -1,11 +1,17 @@
 import { DaumPostcode } from 'components';
 import Icon, { IconFileNames } from 'components/Icon';
+import { LocalStorageKeys } from 'constants/localstorage-keys.constants';
+import { useLocalStorageState } from 'hooks/localstorage';
 import { useState } from 'react';
 import colors from 'styles/palette';
 import { StyledButton, StyledHeader } from './HeaderWithSearchAddress.styles';
 
 export default function HeaderWithSearchAddress() {
   const [showSearchAddress, setShowSearchAddress] = useState(false);
+  const [currentAddress, setCurrentAddress] = useLocalStorageState(
+    LocalStorageKeys.CURRENT_ADDRESS,
+    '서울시 강남구',
+  );
 
   return (
     <StyledHeader>
@@ -14,10 +20,16 @@ export default function HeaderWithSearchAddress() {
           type={IconFileNames.LOCATION_PIN}
           colors={{ black: colors.primaryDark }}
         />
-        동이름(시 구)
+        {currentAddress}
       </StyledButton>
       {showSearchAddress && (
-        <DaumPostcode close={() => setShowSearchAddress(false)} />
+        <DaumPostcode
+          close={() => setShowSearchAddress(false)}
+          onComplete={res => {
+            setCurrentAddress(res.address);
+            setShowSearchAddress(false);
+          }}
+        />
       )}
     </StyledHeader>
   );
