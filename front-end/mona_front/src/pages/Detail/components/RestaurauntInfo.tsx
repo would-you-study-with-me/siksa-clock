e646c0a437423d02b5abac0e6e96909244aa93f7
SoheeP <psh950502@naver.com>
2022-12-26 22:12:54 +0900

    feat: 상세정보 추가

--- front-end/mona_front/src/pages/Detail/components/RestaurauntInfo.tsx
+++ front-end/mona_front/src/pages/Detail/components/RestaurauntInfo.tsx
@@ -1,6 +1,14 @@
 import { useQuery, gql } from '@apollo/client';
 import styled from '@emotion/styled';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CongestionComponent from '../../../components/common/Congestion';
+import StarRate from '../../../components/common/StarRate';
+import {
+  Congestion,
+  RestaurantDetailInfo,
+} from '../../../models/restaurant.model';
 import SlickSlide from './SlickSlide';
 
 const GET_RESTAURANTS_DETAIL = gql`
@@ -33,20 +41,59 @@ const SlideContainer = styled.div`
   height: 30vh;
   overflow: hidden;
 `;
+const CategoryTypo = styled.p`
+  font-size: 0.714rem;
+  font-weight: 400;
+`;
+const TitleTypo = styled(Typography)`
+  padding-bottom: 1rem;
+`;
+const DescriptionTypo = styled(Typography)`
+  padding-bottom: 2rem;
+`;
+const RateAndCongestionContainer = styled.div`
+  display: flex;
+  margin-left: auto;
+  padding-bottom: 1.2rem;
+`;
+const StyledCongestion = styled(CongestionComponent)`
+  margin-left: 3.2rem;
+`;
+const InfoContainer = styled.div``;
 const RestaurauntInfo = () => {
+  const [detailInfo, setDetailInfo] = useState<RestaurantDetailInfo>();
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_RESTAURANTS_DETAIL, {
     variables: {
       id,
     },
+    onCompleted: data => {
+      console.log('on complete', data);
+      setDetailInfo(data.restaurant);
+    },
   });
   if (loading) return <div>로딩</div>;
+
   return (
     <div>
       <SlideContainer>
         <SlickSlide imageUrls={mockImageUrls} />
       </SlideContainer>
-      <div>타이틀과 정보 영역</div>
+      <InfoContainer>
+        <CategoryTypo>
+          {detailInfo?.restaurantCategory ?? '카테고리'}
+        </CategoryTypo>
+        <TitleTypo variant="h2">{detailInfo?.restaurantName}</TitleTypo>
+        <RateAndCongestionContainer>
+          <StarRate rate={detailInfo?.restaurantRate ?? 0} />
+          <StyledCongestion
+            congestion={detailInfo?.restaurantCongestion ?? Congestion.SMOOTH}
+          />
+        </RateAndCongestionContainer>
+        <DescriptionTypo>
+          {detailInfo?.restaurantDescription ?? '식당설명'}
+        </DescriptionTypo>
+      </InfoContainer>
       <div>메뉴 슬라이드</div>
       <div>지도 영역</div>
     </div>
