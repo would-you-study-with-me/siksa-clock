fa89c598b727b8b30e553143a609ce0ab50114aa
MaetDol <rndka9@gmail.com>
2023-01-02 22:29:31 +0900

    헤더가 스크롤되어도 고정될 수 있게 수정

--- front-end/maetdol_front/src/components/HeaderWithSearchAddress/HeaderWithSearchAddress.styles.ts
+++ front-end/maetdol_front/src/components/HeaderWithSearchAddress/HeaderWithSearchAddress.styles.ts
@@ -3,6 +3,9 @@ import styled from '@emotion/styled';
 export const StyledHeader = styled.header`
   padding: 1.6rem;
   border-bottom: 1px solid ${props => props.theme.colors.gray};
+  position: sticky;
+  top: 0;
+  background-color: white;
 `;
 
 export const StyledButton = styled.button`
