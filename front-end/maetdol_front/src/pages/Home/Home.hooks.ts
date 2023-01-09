b53edd6e82d70ab320e55bca1f0a3000b772edfb
MaetDol <rndka9@gmail.com>
2023-01-09 22:21:14 +0900

    레스토랑 카드 리스트 추가로 더 불러올 수 있는 형태로 변경

--- front-end/maetdol_front/src/pages/Home/Home.hooks.ts
+++ front-end/maetdol_front/src/pages/Home/Home.hooks.ts
@@ -1,10 +1,15 @@
 import { gql, useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
 import { RestaurantRawData } from './Home.model';
 import { rawThumbnailDataToThumbnail } from './Home.utils';
 
 const GET_RESTAURANTS_THUMBNAIL_CARDS = gql`
-  query getRestaurantsThumbnailCards($address: String!) {
-    restaurants(inputData: { query: $address }) {
+  query getRestaurantsThumbnailCards(
+    $address: String!
+    $skip: Int!
+    $limit: Int! = 10
+  ) {
+    restaurants(inputData: { query: $address, skip: $skip, limit: $limit }) {
       restaurantCategory
       restaurantId
       restaurantCongestion
@@ -17,14 +22,26 @@ const GET_RESTAURANTS_THUMBNAIL_CARDS = gql`
 `;
 
 export function useRestaurantsThumbnailCard(address: string) {
+  const [cards, setCards] = useState<RestaurantRawData[]>([]);
+  const [offset, setOffset] = useState(0);
+
   const { data, error, loading } = useQuery<
     { restaurants: RestaurantRawData[] },
-    { address: string }
-  >(GET_RESTAURANTS_THUMBNAIL_CARDS, { variables: { address } });
+    { address: string; skip: number }
+  >(GET_RESTAURANTS_THUMBNAIL_CARDS, {
+    variables: { address, skip: offset },
+  });
+
+  useEffect(() => {
+    if (!data?.restaurants.length) return;
+
+    setCards(p => p.concat(data?.restaurants));
+  }, [data?.restaurants]);
 
   return {
-    thumbnailCards: (data?.restaurants ?? []).map(rawThumbnailDataToThumbnail),
+    thumbnailCards: cards.map(rawThumbnailDataToThumbnail),
     error,
     loading,
+    loadMore: () => setOffset(cards.length),
   };
 }
