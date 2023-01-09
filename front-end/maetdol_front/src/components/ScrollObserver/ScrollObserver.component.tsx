160a3839580fe0d095b08c9ec788d96ec0dfe6c9
MaetDol <rndka9@gmail.com>
2023-01-09 21:27:59 +0900

    스크롤을 감지하는 컴포넌트 생성

---
+++ front-end/maetdol_front/src/components/ScrollObserver/ScrollObserver.component.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
+
+interface Props {
+  onIntersect: () => void;
+  bottom: `${number}px` | `${number}rem` | `${number}.${number}rem`;
+}
+
+export default function ScrollObserver({
+  onIntersect,
+  bottom = '0.1rem',
+}: Props) {
+  const [element, setElement] = useState<HTMLSpanElement | null>(null);
+
+  const callbackRef = useRef<Props['onIntersect']>(onIntersect);
+  useEffect(() => {
+    callbackRef.current = onIntersect;
+  }, [onIntersect]);
+
+  useEffect(() => {
+    if (!element) return undefined;
+
+    const observer = new IntersectionObserver(([{ isIntersecting }]) => {
+      if (!isIntersecting) return;
+      callbackRef.current();
+    });
+
+    observer.observe(element);
+    return () => observer.disconnect();
+  }, [element]);
+
+  return (
+    <span
+      ref={setElement}
+      style={{
+        display: 'inline-block',
+        padding: 0,
+        margin: 0,
+        transform: `translateY(${bottom})`,
+      }}
+    />
+  );
+}
--- front-end/maetdol_front/src/components/index.tsx
+++ front-end/maetdol_front/src/components/index.tsx
@@ -3,5 +3,6 @@ export * from './DaumPostcode';
 export { default as HeaderWithBackButton } from './HeaderWithBackButton';
 export { default as Icon } from './Icon';
 export * from './Portal';
+export * from './ScrollObserver';
 export { default as StarRate } from './StarRate';
 export * from './ThumbnailCard';
