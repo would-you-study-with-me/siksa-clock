0bbded55cee52a35a0e3bb770367962a393e8322
jihyundev <aawlgus12@gmail.com>
2023-01-23 22:25:56 +0900

    feat: reverse geocoding fetch 로직 추가

--- front-end/hale_front/src/pages/Main.tsx
+++ front-end/hale_front/src/pages/Main.tsx
@@ -4,38 +4,57 @@ import { useQuery } from '@apollo/client';
 import Typography from '@mui/material/Typography';
 import RestaurantCard from '../components/RestaurantCard';
 import { RestaurantCardInfo } from '../model/restaurant-card.interface';
-import { calculateDistance } from '../utils/util';
 import { Coordinate } from '../model/coordinate.interface';
 import LocationHeader from '../components/LocationHeader';
 import { GET_RESTAURANTS_QUERY } from '../queries/restaurants.query';
+import { GET_REVERSE_GEOCODING_QUERY } from '../queries/geocoding.query';
 
 const Main: React.FC = () => {
-  const location = useLocation();
-  const state = location.state as { roadname: string };
+  // 다음 주소검색 결과값으로 받아온 도로명주소
+  const [addressBySearch, setAddressBySearch] = useState<{
+    roadname: string | null;
+  }>({
+    roadname: null,
+  });
 
-  const [currentCoords, setCurrentCoords] = useState<Coordinate>({
-    latitude: 0,
-    longitude: 0,
+  const location = useLocation();
+  useEffect(() => {
+    const addressData = location.state as { roadname: string };
+    setAddressBySearch(addressData);
+  }, [location.state, addressBySearch]);
+
+  const { refetch } = useQuery(GET_REVERSE_GEOCODING_QUERY, {
+    variables: {
+      x: 127.048542,
+      y: 37.519995,
+    },
   });
 
   useEffect(() => {
+    if (addressBySearch?.roadname) return;
     navigator.geolocation?.getCurrentPosition(
       (position: GeolocationPosition) => {
-        setCurrentCoords({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+        refetch({
+          x: position.coords.longitude,
+          y: position.coords.latitude,
+        }).then(result => {
+          const dongName =
+            result?.data?.reverseGeocoding?.results[0]?.region?.area3.name;
+          // 여기서 레스토랑 refetch
         });
       },
       error => {
         console.error(error);
       },
     );
-  }, []);
+  }, [addressBySearch, refetch]);
 
   const { loading, error, data } = useQuery<{
     restaurants: RestaurantCardInfo[];
   }>(GET_RESTAURANTS_QUERY, {
-    variables: { query: state ? state.roadname : '강남대로' },
+    variables: {
+      query: addressBySearch ? addressBySearch.roadname : '강남대로',
+    },
   });
   if (loading)
     return (
