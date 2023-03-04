3bc371a6c23430362bc47bd366c4aff30c855238
jihyundev <aawlgus12@gmail.com>
2023-03-04 11:32:05 +0900

    feat: 별점 컴포넌트 추가

--- front-end/hale_front/src/components/RestaurantCard.tsx
+++ front-end/hale_front/src/components/RestaurantCard.tsx
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 import { RestaurantCardInfo } from '../model/restaurant-card.interface';
 import Thumbnail from './Thumbnail';
 import CardTitle from './CardTitle';
+import StarRate from './StarRate';
 
 interface RestaurantCardProps extends React.PropsWithChildren {
   name: RestaurantCardInfo['restaurantName'];
@@ -28,7 +29,7 @@ const RestaurantCard = ({
         <CardContent>
           <Thumbnail imageData={image} />
           <CardTitle name={name} />
-          <div>평점: {rate}</div>
+          <StarRate rate={rate} />
           <Typography variant="body1">{category}</Typography>
           <Typography variant="body1">혼잡도</Typography>
           <div>{congestion}</div>
